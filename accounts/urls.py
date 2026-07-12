56f860482f599005229457dbe1fa85d25e8c9678
Aux Court Dev <patelhet.0507@gmail.com>
2026-07-12 11:19:48 +0530

    fix logout to accept GET requests

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', logout_view, name='logout'),
 ]
-
-
